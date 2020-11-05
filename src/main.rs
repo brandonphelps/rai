6f322ae4d6ef32c67501896975d5171aed70eb3e
brandon phelps <bwp44f@mst.edu>
2020-11-04 22:37:46 -0600

    added ident and some functions to reduce box stuff

--- src/main.rs
+++ src/main.rs
@@ -15,22 +15,32 @@ trait Crossover<Rhs=Self> {
     fn crossover(&self, rhs: &Box<Rhs>) -> Self::Output;
 }
 
+use std::sync::atomic::{AtomicUsize, Ordering};
+
+static SinFIdCount: AtomicUsize = AtomicUsize::new(0);
+
 #[derive(Debug)]
 struct SinF {
-    pub value: f64
+    pub value: f64,
+    pub ident: usize
 }
 
 impl SinF {
     // fn crossover(&self, other: &SinF) -> Box<SinF> {
     //     return Box::new(SinF { value: ((self.value + other.value) / 2.0)});
     // }
+
+    fn new(value: f64) -> SinF {
+        let old_count = SinFIdCount.fetch_add(1, Ordering::SeqCst);
+        SinF{value: value, ident: old_count}
+    }
 }
 
 impl Crossover for SinF {
     type Output = SinF;
     fn crossover(&self, _rhs: &Box<SinF>) -> SinF {
         println!("Cross over for SinF");
-        SinF{value: (_rhs.value + self.value) / 2.0}
+        SinF::new((_rhs.value + self.value) / 2.0)
     }
 }
 
@@ -44,7 +54,7 @@ impl Individual for SinF {
     }
 
     fn mutate(&self) -> Box<dyn Individual> {
-        return Box::new(SinF { value: self.value + 0.01});
+        return Box::new(SinF::new(self.value + 0.01));
     }
     
     fn print(&self) -> () {
@@ -65,29 +75,58 @@ fn gen_new_pop<T: Crossover>(individuals: Vec<Box<T>>) -> Vec<T> {
 }
 
 
-fn do_fitness_func<T: Individual>(individuals: Vec<T>) -> () {
+fn do_fitness_func<T: Individual>(individuals: &Vec<T>) -> () {
     for ind in individuals.iter() {
         ind.fitness();
     }
 }
 
+fn select_parents<T: Individual>(individuals: &Vec<T>, parent_count: usize) -> Vec<&T> {
+    let mut parents: Vec<&T> = Vec::new(); 
+    for ind in 1..parent_count  {
+        let rand_f: Option<&T> = individuals.choose(&mut rand::thread_rng());
+        match rand_f {
+            None => panic!("None!"),
+            Some(FD) => {
+                parents.push(FD)
+            },
+        };
+    }
+    return parents;
+}
+
 
 fn main() {
-
     let mut specific_pop: Vec<SinF> = Vec::new();
-
     let mut pop: Vec<Box<dyn Individual>> = Vec::new();
 
     // fitness , index
     let mut results : Vec<(u32, u32)> = Vec::new();
 
-    // generate populateion
+    // generate random populateion
     for n in 1..301 {
-        pop.push(Box::new(SinF{value: (n as f64/100.0)}));
-        specific_pop.push(SinF{value: (n as f64/100.0)});
+        pop.push(Box::new(SinF::new((n as f64/100.0))));
+        specific_pop.push(SinF::new((n as f64/100.0)));
     }
 
-    do_fitness_func(specific_pop);
+    // fitness evaluation
+
+    do_fitness_func(&specific_pop);
+    let mut iteration_count = 0;
+    let max_iter_count = 100;
+
+    while iteration_count < max_iter_count {
+
+        // Select Parents. 
+
+
+
+
+
+        iteration_count += 1;
+    }
+
+
 
     let mut c = 0;
     for n in pop.iter() {
@@ -98,7 +137,7 @@ fn main() {
     // generate fitness values.
 
 
-    // let rand_f: Option<&(u32, u32)> = results.choose(&mut rand::thread_rng());
+
     // let rand_t: Option<&(u32, u32)> = results.choose(&mut rand::thread_rng());
     // let newSinF = match rand_t {
     //     None => panic!("None"),
