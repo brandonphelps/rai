de80af0f7ef0421f592372e1e601bd116e36e290
brandon phelps <bwp44f@mst.edu>
2020-11-07 09:18:35 -0600

    added in bias node id

--- src/nn.rs
+++ src/nn.rs
@@ -21,7 +21,8 @@ struct Edge {
     from_node: u64,
     to_node: u64,
     inno_id: u64,
-    weight: f64
+    weight: f64,
+    enabled: bool
 }
 
 pub struct Network {
@@ -34,6 +35,7 @@ pub struct Network {
     input_node_count: u32,
     output_node_count: u32,
     layer_count: u32,
+    bias_node_id: u64
 }
 
 impl Network {
@@ -42,7 +44,8 @@ impl Network {
                                   edges: Vec::new(),
                                   input_node_count: input_node_count,
                                   output_node_count: output_node_count,
-                                  layer_count: 2};
+                                  layer_count: 2,
+                                  bias_node_id: 0};
         for input_n in 0..input_node_count {
             network.new_node(0);
         }
@@ -50,6 +53,11 @@ impl Network {
         for output_n in 0..output_node_count {
             network.new_node(1);
         }
+
+        // bias node.
+        network.new_node(0);
+
+        network.bias_node_id = (network.nodes.len()-1) as u64;
 
         return network;
     }
@@ -96,17 +104,50 @@ impl Network {
         self.nodes.push(m);
     }
 
+
+    // nod1, nod2
+    // edge <nod1 -- node2>
+    //   edge 0, 1
+    // nod1 --> node3 --> node2
+    
+
+
     /// Takes an edge and inserts a node inline. 
-    pub fn add_node(&mut self, _conn: &Edge) -> () {
-        
+    pub fn add_node(&mut self, _edge_index: usize) -> () {
+        let edge = &mut self.edges[_edge_index];
+        edge.enabled = false;
+
+        let node = &self.nodes[edge.from_node as usize];
+
+        let m = Node{ input_sum: 0.0, output_sum: 0.0, layer: node.layer + 1};
+
+        self.nodes.push(m);
+
+        let edge1 = Edge{from_node: edge.from_node,
+                         to_node: (self.nodes.len() - 1) as u64,
+                         inno_id: 2,
+                         weight: edge.weight,
+                         enabled: true};
+
+        let edge2 = Edge{from_node: edge.from_node,
+                         to_node: (self.nodes.len() - 1) as u64,
+                         inno_id: 2,
+                         weight: edge.weight,
+                         enabled: true};
+
+        self.edges.push(edge1);
+        self.edges.push(edge2);
+            
     }
 
-    pub fn add_connection(&mut self, _node_one: usize, _node_two: usize) -> () {
-        self.edges.push(Edge{from_node: _node_one as u64,
-                             to_node: _node_two as u64,
-                             inno_id: 1,
-                             weight: 0.5}
-                        );
+    pub fn add_connection(&mut self, _node_one: usize, _node_two: usize, weight: f64) -> usize {
+        let edge = Edge{from_node: _node_one as u64,
+                        to_node: _node_two as u64,
+                        inno_id: 1,
+                        weight: weight,
+                        enabled: true};
+        self.edges.push(edge);
+        return self.edges.len() - 1;
     }
 
     fn connect_nodes(&mut self) -> () {
@@ -125,7 +166,7 @@ mod tests {
     fn test_simple_add() {
         let mut network = Network::new(1, 1);
 
-        network.add_connection(0, 1);
+        network.add_connection(0, 1, 0.5);
 
         let input_value = vec![1.0, 2.0];
         let output_values = network.feed_input(input_value);
@@ -134,6 +175,13 @@ mod tests {
         // 1.0 -> node 1 -(0.5)> node 2 -> output (0.5)
         assert_eq!(output_values.len(), 1);
         assert_eq!(output_values[0], 0.5);
+    }
+
+    #[test]
+    fn test_xor_network() {
+        let mut network = Network::new(2, 1);
+
+        
     }
 
     #[test]
