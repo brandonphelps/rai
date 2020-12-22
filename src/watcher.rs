cd3f2fcd2a26cfd33fbe4db1d42e4f26be2c202c
brandon phelps <bwp44f@mst.edu>
2020-12-21 20:11:53 -0600

    added in loading of generation output results

--- src/watcher.rs
+++ src/watcher.rs
@@ -1,7 +1,11 @@
+use std::{env, fs};
+
+
 use sdl2;
 use sdl2::pixels::Color;
 use sdl2::rect::{Point, Rect};
 use sdl2::event::Event;
+use sdl2::EventPump;
 
 use sdl2::keyboard::Keycode;
 use sdl2::render::{Canvas, Texture, TextureCreator};
@@ -92,43 +96,51 @@ fn draw_network(network: &nn::Network, canvas: &mut Canvas<Window>, x_offset: u3
 // draw_network(&player, &mut canvas);
 // canvas.copy(&texture, None, Some(target)).unwrap();
 
-pub fn main() {
-    let sdl_context = sdl2::init().unwrap();
-    let ttf_context = sdl2::ttf::init().unwrap();
-    let video_subsystem = sdl_context.video().unwrap();
-    let window = video_subsystem
-        .window("Window", 800, 600)
-	.position_centered()
-        .build()
-        .unwrap();
+fn read_file(filepath: &str) -> String {
+    let contents = fs::read_to_string(filepath).expect("Somethign went wrong with reading form");
+    return contents;
+}
 
-    fn find_sdl_gl_driver() -> Option<u32> {
-        for (index, item) in sdl2::render::drivers().enumerate() {
-            if item.name == "opengl" {
-                return Some(index as u32);
-            }
-        }
-        None
+struct Generation {
+    individuals: Vec<nn::Network>,
+}
+
+// loads a single playthrough of data from a directory.
+// the directory must be in the form of "generation_num/<list of individuals>"
+// each individual must be on nn::Network type. 
+fn load_playthrough(directory: &str) -> HashMap<u64, Vec<nn::Network>> {
+    let mut results = HashMap::new();
+
+    for entry in fs::read_dir(directory).unwrap() {
+	let entry = entry.unwrap();
+	let path = entry.path();
+	if path.is_dir() {
+	    let gen_id = path.file_name().unwrap().to_str().unwrap().parse::<u64>().unwrap();
+	    let mut individuals = Vec::new();
+	    for individual in fs::read_dir(path).unwrap() {
+		let ind_e = individual.unwrap();
+		let ind_path = ind_e.path();
+		if !ind_path.is_dir() { 
+		    let network_str = read_file(ind_path.as_path().to_str().unwrap());
+		    let network: nn::Network = serde_json::from_str(network_str.as_str()).unwrap();
+		    individuals.push(network);
+		}
+	    }
+
+	    
+	    results.insert(gen_id, individuals);
+	}
     }
 
-    let mut canvas = window
-        .into_canvas()
-	.target_texture()
-	.present_vsync()
-        .build()
-        .unwrap();
+    return results;
+}
 
-    canvas.clear();
 
-    let mut network = nn::Network::new(16, 3, true);
-
-    network.add_node(2, 1.0, 2.0, None);
-
+fn playthrough_asteroids(network: &mut nn::Network, canvas: &mut Canvas<Window>, event_pump: &mut EventPump) {
     let mut asteroids_game = asteroids::game_init();
     let mut duration = 0;
 
     let ten_millis = time::Duration::from_millis(10);
-    let mut event_pump = sdl_context.event_pump().unwrap();
     let mut frame: u32 = 0;
     'running: loop {
         let start = Instant::now();
@@ -147,15 +159,78 @@ pub fn main() {
 	canvas.set_draw_color(Color::RGB(0, 0, 0));
 	canvas.clear();
 
-	draw_network(&network, &mut canvas, 400, 0);
+	draw_network(network, canvas, 400, 0);
 	
-	let game_input = distro::asteroids_thinker(&mut network, &asteroids_game);
+	let game_input = distro::asteroids_thinker(network, &asteroids_game);
 
-	asteroids_game = asteroids::game_update(&asteroids_game, (duration as f64) * 0.01, &game_input, &mut canvas);
+	asteroids_game = asteroids::game_update(&asteroids_game, (duration as f64) * 0.01, &game_input, canvas);
 
+	if asteroids_game.game_over {
+	    break;
+	}
 
 	canvas.present();
         duration = start.elapsed().as_millis();
     }
 
+    
+    // continue displaying the screent, the moment the game is over. 
+    for i in 0..10000 {
+	for event in event_pump.poll_iter() {
+	    match event {
+		Event::Quit { .. }
+		| Event::KeyDown {
+
+		    keycode: Some(Keycode::Escape),
+		    ..
+		} => break,
+		_ => {},
+	    }
+	}
+    }
+}
+
+
+pub fn main() {
+    let args: Vec<String> = env::args().collect();
+    println!("{:?}", args);
+
+    let filename = &args[1];
+    
+    println!("Loading up data from: {}", filename);
+
+    let playthrough = load_playthrough(filename);
+
+    let sdl_context = sdl2::init().unwrap();
+    let ttf_context = sdl2::ttf::init().unwrap();
+    let mut event_pump = sdl_context.event_pump().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+    let window = video_subsystem
+        .window("Window", 800, 600)
+	.position_centered()
+        .build()
+        .unwrap();
+
+    let mut canvas = window
+        .into_canvas()
+	.target_texture()
+	.present_vsync()
+        .build()
+        .unwrap();
+
+    canvas.clear();
+
+    for gen in 3..50 {
+	let network_list = playthrough.get(&gen).unwrap();
+	for (index, network) in network_list.iter().enumerate() { 
+	    if network.fitness() > 100.0 { 
+		println!("Playing generation: {}", gen);
+		println!("playing network: {} which has fitness of {}", index, network.fitness());
+		// let mut network = nn::Network::new(16, 3, true);
+		// network.add_node(2, 1.0, 2.0, None);
+		let mut copy_network = network.clone();
+		playthrough_asteroids(&mut copy_network, &mut canvas, &mut event_pump);
+	    }
+	}
+    }
 }
