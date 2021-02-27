4d411555a5ff8e5675e449aec6fa6eef1e05b248
brandon phelps <bwp44f@mst.edu>
2021-02-26 23:21:16 -0600

    added in scheduler file and moved it from main

--- src/main.rs
+++ src/main.rs
@@ -14,100 +14,22 @@ use beanstalkc::Beanstalkc;
 use serde::{Deserialize, Serialize};
 use serde_json::Result;
 
-use rasteroids::asteroids;
-use rasteroids::collision;
-
+mod scheduler;
 mod distro;
 mod evo_algo;
 mod hrm;
 mod neat;
 mod nn;
 
+use rasteroids::asteroids;
+use rasteroids::collision;
+
+use crate::scheduler::Scheduler;
+
 use std::collections::HashMap;
 use std::time::{Duration, Instant};
 
 use std::sync::atomic::{AtomicUsize, Ordering};
-
-// fmt cares about build ability or something
-// todo: move this.
-struct Scheduler<'a> {
-    current_jobs: Vec<(u128, &'a mut nn::Network)>,
-    job_queue: Beanstalkc,
-    next_job_id: u128,
-}
-
-impl<'a> Scheduler<'a> {
-    // todo: allow for local where beanstalk is not used.
-    pub fn new(host: &str, port: u16) -> Scheduler {
-        let mut p = Beanstalkc::new()
-            .host(host)
-            .port(port)
-            .connect()
-            .expect("Connection failed");
-        p.watch("results");
-        return Scheduler {
-            current_jobs: vec![],
-            job_queue: p,
-            next_job_id: 0,
-        };
-    }
-
-    /// @param: fitness_func_name name of fitness function to run.
-    pub fn schedule_job(
-        &mut self,
-        individual: &'a mut nn::Network,
-        fitness_func_name: &String,
-    ) -> () {
-        self.job_queue.use_tube(&fitness_func_name);
-        let job_id = self.next_job_id + 1 as u128;
-        self.next_job_id += 1;
-
-        let job = distro::JobInfo {
-            name: fitness_func_name.clone(),
-            individual: individual.clone(),
-            job_id: job_id,
-        };
-
-        let job_str = serde_json::to_string(&job).unwrap();
-        match self.job_queue.put(
-            job_str.as_bytes(),
-            1,
-            Duration::from_secs(0),
-            Duration::from_secs(120),
-        ) {
-            Ok(t) => self.current_jobs.push((job_id, individual)),
-            Err(_) => {
-                println!("Failed to schedule job")
-            }
-        };
-    }
-
-    pub fn wait(&mut self) -> () {
-        // hold off or do w/e till scheduled items are finished.
-        while self.current_jobs.len() > 0 {
-            let mut current_job = self.job_queue.reserve_with_timeout(Duration::from_secs(2));
-            match current_job {
-                Ok(mut job_info) => {
-                    job_info.delete();
-                    // self.job_queue.delete(current_job.id());
-                    // todo: pair fitness with the scheduled fitness items.
-                    let mut i = 0;
-                    let unpacked_result: distro::JobResults =
-                        serde_json::from_slice(&job_info.body()).unwrap();
-                    for (index, job_r) in self.current_jobs.iter().enumerate() {
-                        if unpacked_result.job_id == job_r.0 {
-                            i = index;
-                        }
-                    }
-                    let mut queued_job = self.current_jobs.remove(i);
-                    queued_job.1.fitness = unpacked_result.fitness;
-                }
-
-                Err(_) => (),
-            }
-        }
-    }
-}
 
 fn run_ea(
     input_count: u32,
