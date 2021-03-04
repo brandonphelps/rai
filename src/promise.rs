93a0767cb20812a9adf2db507b5e66be861ed016
brandon phelps <bwp44f@mst.edu>
2021-03-04 02:50:28 -0600

    refactor, removed generic for EAFuture input, removed old example code stuff

--- src/promise.rs
+++ src/promise.rs
@@ -11,41 +11,19 @@ trait Individual : Clone {
 
 trait IndividualFuture<T> where T: Individual {
     type Output;
-    fn poll_s(&mut self, sched: &mut IndividualScheduler<T>) -> Poll<Self::Output>;
+    fn poll(&mut self, sched: &mut IndividualScheduler<T>) -> Poll<Self::Output>;
 }
 
-
-trait Future {
-    type Output;
-
-    fn poll(&mut self, cx: &Context) -> Poll<Self::Output>;
-    fn poll_n(&mut self) -> Poll<Self::Output>;
-    fn poll_s<S: Individual>(&mut self, sched: &mut IndividualScheduler<S>)-> Poll<Self::Output>;
-}
-
-#[derive(Default)]
-struct MyFuture {
-    id: u32,
-    count: u32,
-}
-
-impl MyFuture { 
-  pub fn new(id: u32) -> Self { 
-    Self { id: id, count: 0 }
-  }
-}
-
-
-struct EAFuture<T>  where T: Individual { 
-    // can input be removed? 
-    input: T,
+// todo: should this be generic'ed on the Output
+// and store the output here? 
+struct EAFuture { 
     result: f64,
     job_id: u32,
 }
 
-impl<T> EAFuture<T> where T: Individual {
-    fn new(input: T) -> Self {
-	Self { input: input, result: 0.0, job_id: 0 }
+impl EAFuture { 
+    fn new() -> Self {
+	Self { result: 0.0, job_id: 0 }
     }
     
     pub fn set_id(&mut self, id: u32) {
@@ -72,20 +50,16 @@ impl<T> IndividualScheduler<T> where T: Individual {
 	}
     }
 
-    pub fn schedule_job(&mut self, job_info: T) -> EAFuture<T> {
-	let mut f = EAFuture::new(job_info.clone());
+    pub fn schedule_job(&mut self, job_info: T) -> EAFuture {
+	let mut f = EAFuture::new();
 	f.set_id(self.input.len() as u32);
 	self.output.push(None);
 	self.input.push(job_info);
 	return f;
     }
-   
-    pub fn do_handling(&self, f: &mut EAFuture<T>) {
-	f.result = self.output.len() as f64;
-    }
 
     // should f be mut ? 
-    pub fn get_result(&self, f: &EAFuture<T>) -> Option<f64> {
+    pub fn get_result(&self, f: &EAFuture) -> Option<f64> {
 	if f.get_id() as usize >= self.output.len() {
 	    None
 	} else { 
@@ -121,13 +95,13 @@ impl<T> IndividualScheduler<T> where T: Individual {
     }
 }
 
-impl<T> IndividualFuture<T> for EAFuture<T>
+impl<T> IndividualFuture<T> for EAFuture
 where
     T: Individual
 {
     type Output = f64;
 
-    fn poll_s(&mut self, sched: &mut IndividualScheduler<T>) -> Poll<Self::Output> {
+    fn poll(&mut self, sched: &mut IndividualScheduler<T>) -> Poll<Self::Output> {
 	match sched.get_result(&self) {
 	    Some(t) => {
 		Poll::Ready(t)
@@ -135,33 +109,6 @@ where
 	    None => { Poll::Pending },
 	}
     }
-}
-
-// scheduler.
-fn run_many<F>(mut f: Vec::<F>) -> Vec::<F::Output>
-where
-    F: Future,
-{
-    // create beanstalkd connection.
-    // let mut scheduler = Beanstalkd::new();
-    // for i in f.iter_mut() {
-    //     scheduler.schedule_job(&mut i);
-    // }
-
-    let mut results = Vec::<F::Output>::new();
-    while results.len() < f.len() { 
-        for i in f.iter_mut() {
-            match i.poll_n() { 
-		Poll::Ready(val) => { 
-                    results.push(val);
-		}, 
-		Poll::Pending => {
-                    println!("Waiting around");
-		}
-            }
-	}
-    }
-    return results;
 }
 
 enum JobState {
@@ -204,7 +151,7 @@ mod tests {
 	sched.wait();
 
 	// all associated futures must be completed since we did a wait. 
-	match p.poll_s(&mut sched) {
+	match p.poll(&mut sched) {
 	    Poll::Ready(value) => {
 		println!("Got a value: {}", value);
 		assert!(true);
@@ -215,7 +162,7 @@ mod tests {
 	    }
 
 	}	    
-	match j.poll_s(&mut sched) {
+	match j.poll(&mut sched) {
 	    Poll::Ready(value) => {
 		println!("Got a value: {}", value);
 		assert!(true);
