5cbda1539a2846ae02f495625680b648db093c2b
brandon phelps <bwp44f@mst.edu>
2021-03-05 02:21:47 -0600

    added in individual file

---
+++ src/individual.rs
@@ -0,0 +1,10 @@
+
+
+
+pub trait Individual : Clone + Debug {
+    // can this return just a numeric traited instance?
+    // post calculated fitness.
+    fn fitness(&self) -> f64;
+    fn ea_name(&self) -> String;
+}
+
