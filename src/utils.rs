68467bc14c867782e9f6aa289ec1c2ba08933ea6
brandon phelps <bwp44f@mst.edu>
2021-03-09 06:04:53 -0600

    added utils file

---
+++ src/utils.rs
@@ -0,0 +1,15 @@
+
+/// Helper functions that aren't specific to any module
+
+use std::collections::HashSet;
+use std::hash::Hash;
+
+pub fn has_unique_elements<T>(iter: T) -> bool
+where
+    T: IntoIterator,
+    T::Item: Eq + Hash
+{
+    let mut uniq = HashSet::new();
+    iter.into_iter().all(move |x| uniq.insert(x))
+}
+
